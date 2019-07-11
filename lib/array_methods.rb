fb9b533f97d02326dcbdeaaa56b6214682f57703
rabab raza ali <robin.ali85@yahoo.com>
2019-07-11 20:41:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,22 +3,22 @@ array.include? (element)
 end 
 
 def using_sort (array, element)
-
+array.sort
 end
 
 
 def using_ reverse(array, element)
-
+array.reverse
 end
 
 def using_first (array, element)
-
+array.first
 end
 
 def using_last (array, element)
-  
+  array.last
 end
 
 def using_size (array, element)
-  
+  array.size
 end
