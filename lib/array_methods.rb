58922c1032851a2e2deadf6691dfd99c3598a24b
rabab raza ali <robin.ali85@yahoo.com>
2019-07-11 20:31:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,9 @@
 def using_include(array, element)
-
-end
-
-def using_sort(array)
-
-end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
+array.include? (element)
+end 
+]
 
 def using_size(array)
-
+array.size (element) 
 end
+
